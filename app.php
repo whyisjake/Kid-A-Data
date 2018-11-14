2d3c541b53a28c1736e7f68eb4c61bc8e64400dc
Jake Spurlock <whyisjake@gmail.com>
2018-11-14 12:57:59 -0800

    Some more cleanup

--- app.php
+++ app.php
@@ -1,6 +1,17 @@
 <?php
+/**
+ * Spotify App
+ *
+ * @category Spotify
+ * @package  Kid-A
+ * @author   Jake Spurlock <jspurloc@usc.edu>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @version  GIT: $Id$
+ * @link     https://github.com/whyisjake/Kid-A-Data
+ */
 
 require 'vendor/autoload.php';
+require 'albumData.php';
 
 $session = new SpotifyWebAPI\Session(
     '',
@@ -8,85 +19,91 @@ $session = new SpotifyWebAPI\Session(
 );
 
 $session->requestCredentialsToken();
-$accessToken = $session->getAccessToken();
+$access_token = $session->getAccessToken();
 
 $api = new SpotifyWebAPI\SpotifyWebAPI();
-$api->setAccessToken($accessToken);
+$api->setAccessToken( $access_token );
 
-require 'albumData.php';
+$new_data = [];
 
-$newData = [];
+foreach ( $data as $album_data ) {
 
-foreach ( $data as $albumData ) {
+	// Build the search query.
+	$search_query = sprintf(
+		'artist:%s album:%s',
+		str_replace( '\'', '', $album_data['artist'] ),
+		str_replace( '\'', '', $album_data['album'] )
+	);
 
-    // Build the Search Query
-    $searchQuery = sprintf(
-        'artist:%s album:%s',
-        str_replace( '\'', '', $albumData['artist'] ),
-        str_replace( '\'', '', $albumData['album'] ) );
-
-    // Execute the search
-    $albumSearch = $api->search( $searchQuery, 'album' );
-    $album = $albumSearch->albums->items[0];
+	// Execute the search.
+	$album_search = $api->search( $search_query, 'album' );
+	$album        = $album_search->albums->items[0];
 
 
-    // Setup some default data.
-    $albumData['searchParam'] = $searchQuery;
-    $albumData['artist']      = $album->artists[0]->name;
-    $albumData['album']       = $album->name;
-    $albumData['@url']        = $album->images[0]->url;
+	// Setup some default data.
+	$album_data['searchParam'] = $search_query;
+	$album_data['artist']      = $album->artists[0]->name;
+	$album_data['album']       = $album->name;
+	$album_data['@url']        = $album->images[0]->url;
 
-    $filename = preg_replace("/[^a-z0-9\.]/", "", strtolower( $albumData['album'] ) );
-    $albumData['@art'] = sprintf( 'tmp/%s.jpg', $filename );
+	$filename           = preg_replace( '/[^a-z0-9\.]/', '', strtolower( $album_data['album'] ) );
+	$album_data['@art'] = sprintf( 'tmp/%s.jpg', $filename );
 
-    // Save the album art
-    getAlbumArt( $album->images[0]->url, sprintf( '%s.jpg', $filename ) );
+	// Save the album art.
+	get_album_art( $album->images[0]->url, sprintf( '%s.jpg', $filename ) );
 
-    $albumData['primaryColor'] = getImageColor( $albumData['@art'] )[0];
-    $albumData['secondaryColor'] = getImageColor( $albumData['@art'] )[1];
+	$album_data['primaryColor']   = get_image_color( $album_data['@art'] )[0];
+	$album_data['secondaryColor'] = get_image_color( $album_data['@art'] )[1];
 
-    error_log( $albumData['@art'] );
-    error_log( $albumData['primaryColor'] );
+	// Let's get the count of the cards.
+	$count = $album_data['count'];
 
-    // Let's get the count of the cards.
-    $count = $albumData['count'];
-
-    // The data is going to be the same here, don't need to dulicate all of the functions.
-    for ( $i=0; $i < $count; $i++ ) {
-
-        // Append to the new array.
-        $newData[] = $albumData;
-    }
+	// The data is going to be the same here, don't need to dulicate all of the functions.
+	for ( $i = 0; $i < $count; $i++ ) {
+		// Append to the new array.
+		$new_data[] = $album_data;
+	}
 }
 
-// Start the CSV Output
-$output = fopen("php://output",'w') or die("Can't open php://output");
-header("Content-Type:application/csv");
-header("Content-Disposition:attachment;filename=albumData.csv");
+// Start the csv output.
+$output = fopen( 'php://output', 'w' ) || die( "Can't open php://output" );
+header( 'Content-Type:application/csv' );
+header( 'Content-Disposition:attachment;filename=albumData.csv' );
 
 // Output the header.
-$headers = array('artist', 'album', 'number', 'decade', 'count', 'primaryColor', 'secondaryColor', '@art', 'searchParam', '@url' );
+$headers = array( 'artist', 'album', 'number', 'decade', 'count', 'primaryColor', 'secondaryColor', '@art', 'searchParam', '@url' );
 fputcsv( $output, $headers );
 
-foreach($newData as $product) {
-    fputcsv($output, $product);
+foreach ( $new_data as $product ) {
+	fputcsv( $output, $product );
 }
 
 // We done here...
-fclose( $output ) or die( "Can't close php://output" );
+fclose( $output ) || die( "Can't close php://output" );
 
 
 /**
+ * Save the Album Art
  *
-*/
-function getAlbumArt( $url = '', $name = '' ) {
-    $img = sprintf( '%s/tmp/%s', getcwd(), $name );
-    file_put_contents( $img, file_get_contents( $url ) );
+ * @param string $url  Remote URL to save.
+ * @param string $name File name.
+ *
+ * @return void
+ */
+function get_album_art( $url = '', $name = '' ) {
+	$img = sprintf( '%s/tmp/%s', getcwd(), $name );
+	file_put_contents( $img, file_get_contents( $url ) );
 }
 
-
-function getImageColor( $url ) {
-    // initiate with image
-    $palette = new \BrianMcdo\ImagePalette\ImagePalette( $url );
-    return $palette->colors;
+/**
+ * Get the color palette for an image.
+ *
+ * @param string $url Local image path.
+ *
+ * @return array
+ */
+function get_image_color( $url = '' ) {
+	// Initiate with image.
+	$palette = new \BrianMcdo\ImagePalette\ImagePalette( $url );
+	return $palette->colors;
 }
